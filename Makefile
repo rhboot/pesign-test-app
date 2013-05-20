8865309b499c276585163f048f83c4d859415863
Peter Jones <pjones@redhat.com>
2013-05-20 17:19:19 -0400

    Add code, license, and Makefile.
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ Makefile
@@ -0,0 +1,26 @@
+PREFIX := /
+DATADIR := /usr/share
+CC = gcc
+CFLAGS = -O2 -fpic -Wall -fshort-wchar -fno-strict-aliasing -fno-merge-constants -mno-red-zone -DCONFIG_x86_64 -DGNU_EFI_USE_MS_ABI -maccumulate-outgoing-args --std=c99 -I/usr/include/efi -I/usr/include/efi/x86_64 -I/usr/include/efi/protocol
+LD = ld
+LDFLAGS = -nostdlib -T /usr/lib64/gnuefi/elf_x86_64_efi.lds -shared -Bsymbolic -L/usr/lib64 /usr/lib64/gnuefi/crt0-efi-x86_64.o
+OBJCOPY = objcopy
+
+all : pesign-test-app.efi
+
+%.efi : %.so
+	$(OBJCOPY) -j .text -j .sdata -j .data -j .dynamic -j .dynsym -j .rel \
+		   -j .rela -j .reloc --target=efi-app-x86_64 $^ $@
+
+%.so : %.o
+	$(LD) $(LDFLAGS) -o $@ $^ -lefi -lgnuefi
+
+%.o : %.c
+	$(CC) $(CFLAGS) -c -o $@ $^
+
+clean :
+	@rm -vf *.o *.so *.efi
+
+install :
+	install -D -d -m 0755 $(PREFIX)/$(DATADIR)/pesign-test-app
+	install -m 0644 pesign-test-app.efi $(PREFIX)/$(DATADIR)/pesign-test-app/pesign-test-app.efi
---
+++ pesign-test-app.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author(s): Peter Jones <pjones@redhat.com>
+ */
+
+
+#include <stdint.h>
+
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS
+efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *systab)
+{
+	InitializeLib(image_handle, systab);
+
+	Print(L"This is a test application that should be completely safe.\n");
+
+	return EFI_SUCCESS;
+}
